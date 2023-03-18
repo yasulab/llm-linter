30d77d0dbefb14a6ed1aad8ffa8a86d6d47f8e3f
Yohei Yasukawa <yohei@yasslab.jp>
2023-03-19 03:15:58 +0900

    細かなコードのリファクタリングと両モデルの互換対応

--- app.rb
+++ app.rb
@@ -44,8 +44,9 @@ def failed_longer_than_200; "# AI からのコメント\n\nすみません！概
 def failed_after_deadline;  "# AI からのコメント\n\nすみません！2023年度の未踏ジュニア応募〆切は2023年4月8日 23:59 までとなるため、それに伴って実験的な本システムの提供も終了いたしました。\n\nあらためて、未踏ジュニアに興味を持っていただきありがとうございました！" end
 
 def is_after_deadline?
-  puts t1 = Time.new(2023, 4, 8, 23, 59, 59, "+09:00")
-  puts t2 = Time.now.localtime("+09:00")
+  t1 = Time.new(2023, 4, 8, 23, 59, 59, "+09:00")
+  t2 = Time.now.localtime("+09:00")
+  p(t2)
   t1 < t2
 end
 
@@ -54,36 +55,53 @@ def chat_gpt_request(user_query)
   return failed_longer_than_200 if user_query.size > 200
   return failed_after_deadline  if is_after_deadline?
 
-  prompt   = get_prompt()
-  client   = OpenAI::Client.new
-  response = client.chat(
-                    parameters: {
-                           # https://platform.openai.com/docs/models/gpt-4
-                           #model:  'gpt-3.5-turbo',
-                           model:  'gpt-4',
-                           messages: [
-                             {
-                               role: 'system',
-                               content: prompt,
-                             },
-                             {
-                               role: 'user',
-                               content: user_query,
-                             },
-                           ],
-                           temperature: 0.7,
-                           #stream: True,
-                         }
-                    )
+  prompt = get_prompt()
+  client = OpenAI::Client.new
+  model  = 'gpt-4'
+  #model  = 'gpt-3.5-turbo'
+  params = {
+    # https://platform.openai.com/docs/models/gpt-4
+    model: model,
+    messages: [
+      {
+        role: 'system',
+        content: prompt,
+      },
+      {
+        role: 'user',
+        content: user_query,
+      },
+    ],
+    temperature: 0.7,
+    #stream: True,
+  }
+  response = client.chat(parameters: params)
 
-  d2y = 135
   p_tokens = response.dig 'usage', 'prompt_tokens'
   c_tokens = response.dig 'usage', 'completion_tokens'
-  expenses = (p_tokens * 0.03 / 1000) + (c_tokens * 0.06 / 1000) # dollars
-  puts "Prmpt Tokens: " + p_tokens.to_s + " (#{(p_tokens * 0.03 / 1000 * d2y).floor(2)}円)"
-  puts "Cmplt Tokens: " + c_tokens.to_s + " (#{(c_tokens * 0.06 / 1000 * d2y).floor(2)}円)"
-  puts "掛かった料金: " + (expenses * 135).floor(2).to_s + "円"
+  t_tokens = response.dig 'usage', 'total_tokens'
+
+  if model == 'gpt-4'
+    # Expenses on GPT-4 (8K context): https://openai.com/pricing
+    p_expenses = (p_tokens * 0.03 / 1000) # dollars
+    c_expenses = (c_tokens * 0.06 / 1000) # dollars
+    puts "Prmpt Tokens: #{p_tokens} (#{(p_expenses).to_yen.floor(2)}円)"
+    puts "Cmplt Tokens: #{c_tokens} (#{(c_expenses).to_yen.floor(2)}円)"
+    puts "掛かった料金: #{(p_expenses + c_expenses).to_yen.floor(2)}円"
+  else
+    # Expenses on GPT-3.5-turbo: https://openai.com/pricing
+    p_expenses = (p_tokens * 0.002 / 1000) # dollars
+    c_expenses = (c_tokens * 0.002 / 1000) # dollars
+    puts "Prmpt Tokens: #{p_tokens} (#{(p_expenses).to_yen.floor(2)}円)"
+    puts "Cmplt Tokens: #{c_tokens} (#{(c_expenses).to_yen.floor(2)}円)"
+    puts "掛かった料金: #{(p_expenses + c_expenses).to_yen.floor(2)}円"
+  end
+  puts "使ったモデル: " + model
 
   response.dig('choices', 0, 'message', 'content')
+end
+
+class Float
+  def to_yen; self * 135; end
 end
 
