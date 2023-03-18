0088ce568a0db5367ca16eba94e62a7db98102d1
Yohei Yasukawa <yohei@yasslab.jp>
2023-03-18 23:06:27 +0900

    Set value proposition on abstract ONLY instead of general sentences

--- app.rb
+++ app.rb
@@ -29,24 +29,27 @@ end
 
 def get_prompt
   <<-HINT_FOR_AI
-  You are a tutor that checks Japanese sentences on a project proposed by young creator who are willing to apply "未踏ジュニア" program. Do the following step by step:
+  You are a tutor that feedbacks on a abstract of project, called "概要文", proposed by young creator who are willing to apply "未踏ジュニア" program. The abstract has to be within 200 words in Japanese.
 
-  1. Praise their work first in your words.
-  2. Show a few ideas that they can do to make the sentences more explicitly.
-  3. Give one example sentence that follows the ideas but keep their taste remain and never modify their project for their creativity.
+  Show a few ideas that they may make it more attractive but keep their taste remain and *never* modify their project for their creativity. Keep in mind that the 200 words is too short to follow all of your ideas.
 
-  Output has to be within 500 words in Japanese, started with '# AI からの文章フィードバック', and formatted in Markdown.
+  Output has to be *a suggestion* in Japanese, started with '# AI によるコメント', and formatted in Markdown. If you show your understanding on the input, you should start with like "My understanding of your project is ..."
 
+  Learn how to count Japanese words by using the following examples:
+
+  * 21 words: 自宅で手軽に筋トレができるVRソフトです。
+  * 41 words: 逆方向に動く2つのキューブを同時にゴールに持っていく、シンプルなパズルゲームです。
+  * 52 words: 「Visible」はNode.jsで開発されるオープンソースのWebアクセシビリティーテストツールです。
 
   HINT_FOR_AI
 end
 
 def failed_no_inputs_given; "# AI からの文章フィードバック\n\n未踏ジュニアに興味を持っていただきありがとうございます！私は提案書の概要や、提案書の文章をチェックする AI です。\n\n「サンプル文章を入力する」ボタンを押してから、「AI の文章をみてもらう」ボタンを押すと、私の回答例を確認できます。ぜひ試してみてくださいね！" end
-def failed_longer_than_500; "# AI からの文章フィードバック\n\nすみません！私が一度に見れる文章は500文字までとなります。500文字以内に区切ってから、再度「AI の文章をみてもらう」ボタンを押していただけると嬉しいです。" end
+def failed_longer_than_200; "# AI からの文章フィードバック\n\nすみません！概要文は200文字以内となります。200文字以内に収めてから、再度「AI の文章をみてもらう」ボタンを押していただけると嬉しいです。" end
 
 def chat_gpt_request(user_query)
   return failed_no_inputs_given if user_query.size == 0
-  return failed_longer_than_500 if user_query.size > 500
+  return failed_longer_than_200 if user_query.size > 200
   prompt   = get_prompt()
   client   = OpenAI::Client.new
   response = client.chat(
@@ -69,9 +72,9 @@ def chat_gpt_request(user_query)
                          }
                     )
 
-  p_tokens = response.dig('usage', 'prompt_tokens'
-  c_tokens = response.dig('usage', 'completion_tokens')
-  t_tokens = response.dig('usage', 'total_tokens')
+  p_tokens = response.dig 'usage', 'prompt_tokens'
+  c_tokens = response.dig 'usage', 'completion_tokens'
+  t_tokens = response.dig 'usage', 'total_tokens'
   expenses = (p_tokens * 0.03 / 1000) + (c_tokens * 0.06 / 1000) # dollars
   puts "Prmpt Tokens: " + p_tokens.to_s
   puts "Cmplt Tokens: " + c_tokens.to_s
